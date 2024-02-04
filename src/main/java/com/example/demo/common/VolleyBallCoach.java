adcaff8de010a52e28e4edb9334e38d58ea5c745
ronieldegozo <ronieldegozo@gmail.com>
2024-02-04 15:46:41 -0800

    Adding loggins for Post and PreDestroy (PreDrestroy which means destroy or close the application)

--- src/main/java/com/example/demo/common/VolleyBallCoach.java
+++ src/main/java/com/example/demo/common/VolleyBallCoach.java
@@ -1,5 +1,7 @@
 package com.example.demo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,19 @@ public class VolleyBallCoach  implements Coach{
     public VolleyBallCoach(){
         System.out.println("In Constructor: " + getClass().getSimpleName());
     }
+
+    //Define init method
+    @PostConstruct
+    public void testPostConstruct(){
+        System.out.println("In Testing for PostContruct: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void testPreDestroy(){
+        System.out.println("In Testing for PreDestroy: " + getClass().getSimpleName());
+    }
+
+
     @Override
     public String getDailyWorkOut() {
         return "Jump for 1 hr, so you can jump High!";
